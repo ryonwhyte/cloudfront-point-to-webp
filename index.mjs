55d70ae2c332c02a791c6d42e65b0c236e17e1f9
Ryon Whyte <ryon@sitepact.com>
2024-08-06 16:19:52 -0500

    Library Change
    Change library from useragent to ua-parser-js which appear to be much better maintained

--- index.mjs
+++ index.mjs
@@ -1,37 +1,48 @@
+import UAParser from 'ua-parser-js';
+import path from 'path';
 
-import useragent  from 'useragent';
-import path from 'path'
+export const handler = async (event, context, callback) => {
+  const request = event.Records[0].cf.request;
+  const headers = request.headers;
+  const userAgentString = headers['user-agent'] && headers['user-agent'][0] ? headers['user-agent'][0].value : null;
 
-exports.handler = async (event, context, callback) => {
-  const request = event.Records[0].cf.request
-  const headers = request.headers
-  const userAgentString = headers['user-agent'] && headers['user-agent'][0] ? headers['user-agent'][0].value : null
-  const agent = useragent.lookup(userAgentString)
+  // Initialize UA parser and get result from userAgentString
+  const parser = new UAParser();
+  const agent = parser.setUA(userAgentString).getResult();
+
+  console.log("New Agent is: ", agent);
 
   const browsersToInclude = [
     { browser: 'Chrome', version: 23 },
-    { browser: 'Opera', version: 15 },
-    { browser: 'Android', version: 53 },
+    { browser: 'Edge', version: 18 },
+    { browser: 'Safari', version: 14 },
+    { browser: 'Firefox', version: 65 },
+    { browser: 'Opera', version: 12 },
     { browser: 'Chrome Mobile', version: 55 },
+    { browser: 'Android', version: 53 },
+    { browser: 'Mobile Safari', version: 14 },
     { browser: 'Opera Mobile', version: 37 },
     { browser: 'UC Browser', version: 11 },
-    { browser: 'Samsung Internet', version: 4 }
-  ]
+    { browser: 'Samsung Internet', version: 4 },
+    { browser: 'Baidu', version: 13 },
+    { browser: 'KaiOS', version: 3 },
+    { browser: 'Opera [GX/Mini/Mobi/Tablet]', version: 1 }
+  ];
 
   const supportingBrowser = browsersToInclude
-    .find(browser => browser.browser === agent.family && agent.major >= browser.version)
-    
-  if (supportingBrowser) {
-    const fileFormat = path.extname(request.uri).replace('.', '')
+    .find(browser => browser.browser === agent.browser.name && parseInt(agent.browser.version, 10) >= browser.version);
+
+  if (supportingBrowser) {  // Check if the browser is supported
+    const fileFormat = path.extname(request.uri).replace('.', '');
     request.headers['original-resource-type'] = [{
       key: 'Original-Resource-Type',
       value: `image/${fileFormat}`
-    }]
+    }];
 
-    const olduri = request.uri
-    const newuri = olduri.replace(/(\.jpg|\.png|\.jpeg)$/g, '.webp')
+    const olduri = request.uri;
+    const newuri = olduri.replace(/(\.jpg|\.png|\.jpeg)$/g, '.webp');
     request.uri = newuri.replace('/original/', '/optimized/');
   }
 
-  return callback(null, request)
-}
+  return callback(null, request);
+};
